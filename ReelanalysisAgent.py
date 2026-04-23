0189c56087f887cb44f825fd7d648b718067b90b
takinosuke <myktig.172@gmail.com>
2026-04-24 01:35:34 +0900

    fix display message

--- ReelanalysisAgent.py
+++ ReelanalysisAgent.py
@@ -216,8 +216,10 @@ def identifyUserNeeds(type, play_num, analysis_genre):
         usage_usd = run_details.get("usage", {}).get("totalChargeUsd", 0)
         # 3. アクター内で明示的に定義された「アドオン」的な課金
         usage_extra = run_details.get("usage", {}).get("extraBilling", 0)
+        usage_ori = run_details.get("stats", {}).get("computeUnits", 0)
         st.write(usage_usd)
         st.write(usage_extra)
+        st.write(usage_ori)
         st.json(run_details.get("usage"))
         # Streamlitのセッション状態に加算
         st.session_state.apify_token += usage_cu
